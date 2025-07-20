52edd851521bb29d6a36e1fc23b63084067c9202
Ismoil-21 <toxirovi82@gmail.com>
2025-07-20 16:07:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+// let ovoz = prompt("Hayvon nomi")
+
+// function bot (a) {
+//     if(a === "IT"){
+//         alert("WOWOWO!")
+//     }else if(a === "Mushuk"){
+//         alert("Miyauv")
+//     }else if(a === ""){
+//         alert("Hayvon nomini kiriting!")
+//     }else{
+//         alert("Ushbu hayvon nomi mavjud emas")
+//     }
+// }
+// bot(ovoz);
+// let raqam = +prompt("Son kiriting")
+
+// function JuftsonToqson(a){
+//     if(a === "" || a === null){
+//         alert("Iltimos son kiriting")
+//     }else if(a % 2 == 0){
+//         alert(`${a} soni juft`)
+//     }else{
+//         alert(`${a} soni toq`)
+//     }
+// }
+// JuftsonToqson(raqam)
+
+
+// function bos(){
+//     document.body.style.backgroundColor = "black"
+// }
+
+// function orqaga(){
+//     document.body.style.backgroundColor = "white"
+// }
+
+function ismoil(){
+    document.body.classList.add("dark-mode");
+}
+
+
+function kiss(){
+    document.body.classList.remove("dark-mode");
+}
